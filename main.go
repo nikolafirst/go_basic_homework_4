82f19e72b4d51fe0658d17112d4784de80f6977f
Nikolay Karasev <nomotorolla@gmail.com>
2024-05-02 10:05:44 +0300

    solved

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Cache is a simple in-memory key value store
+type Cache struct {
+	data map[string]interface{}
+	mu   sync.Mutex
+}
+
+// NewCache creates a new Cache instance
+func NewCache() *Cache {
+	return &Cache{
+		data: make(map[string]interface{}),
+	}
+}
+
+// Set adds a new key value pair to the cache
+func (c *Cache) Set(key string, value interface{}) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.data[key] = value
+}
+
+// Get retrieves a value from the cache by its key
+func (c *Cache) Get(key string) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	value, ok := c.data[key]
+	return value, ok
+}
+
+func main() {
+	cache := NewCache()
+
+	cache.Set("key1", "value1")
+	cache.Set("key2", 123)
+
+	val1, ok1 := cache.Get("key1")
+	fmt.Println("Value1:", val1, "Exists:", ok1)
+
+	val3, ok3 := cache.Get("key3")
+	fmt.Println("Value3:", val3, "Exists:", ok3)
+}
